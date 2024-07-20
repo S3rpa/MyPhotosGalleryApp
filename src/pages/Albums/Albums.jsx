176e96f35b9be3e590b162c8aa732ec37f5aebc1
S3rpa <sergiobarbera1@gmail.com>
2024-07-21 00:36:28 +0200

    add localStorage:favs and remove and download

--- src/pages/Albums/Albums.jsx
+++ src/pages/Albums/Albums.jsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import { useContext } from 'react';
+import React,{ useContext } from 'react';
 import { useSelector } from 'react-redux';
 import { FavoritesContext } from '../../features/favs/favs';
 import ImgCard from '../../components/RandomImagesCard/card';
 import { selectSearch } from '../../features/search/searchSlice';
 
 const Albums = () => {
-  const { favorites } = useContext(FavoritesContext);
+  const { favorites,removeFavorite } = useContext(FavoritesContext);
   const searchTerm = useSelector(selectSearch);
 
   // Filtrar favoritos según el término de búsqueda
-  const filteredFavorites = searchTerm
-    ? favorites.filter(image =>
-        image.alt.toLowerCase().includes(searchTerm.toLowerCase()))
-    : favorites;
+    const filteredFavorites = searchTerm
+        ? favorites.filter(image =>
+            image.alt.toLowerCase().includes(searchTerm.toLowerCase()))
+        : favorites;
 
-  return (
-    <div>
-      <ImgCard images={filteredFavorites} />
-    </div>
-  );
+    
+    const handleRemove = (id) => {
+        removeFavorite(id);
+        if (onRemove) {
+          onRemove(id);
+        }
+    };
+      
+    return (
+        <div>
+            <ImgCard images={filteredFavorites} onRemove={handleRemove} showRemoveAndDownload/>
+        </div>
+    );
 };
 
 export default Albums;
